71920742a496ffbb8702b6eb7152177f104f8ca4
Daniel Morley <morleydan10@gmail.com>
2024-06-09 11:51:08 -0400

    Added minor changes to page, still wireframing website

---
+++ src/app/AboutMe.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function AboutMe() {
+
+    return(
+        <section>
+            <div>
+                <p>This is the about section</p>
+            </div>
+        </section>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import Header from "./Header";
+import AboutMe from "./AboutMe";
 
 function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 dark:bg-slate-800">
       <Header/>
+      <AboutMe /> 
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
         </div>
